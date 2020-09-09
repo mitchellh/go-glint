a678c1e33096dda352472918d398864d2076bba3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-09-09 09:28:28 -0700

    update sparkline example

--- examples/sparkline.go
+++ examples/sparkline.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
+	"sync/atomic"
 	"time"
 
 	"github.com/mitchellh/go-glint"
@@ -13,7 +15,7 @@ func main() {
 	max := 25
 	min := 1
 
-	values := make([]uint, 25)
+	values := make([]uint, 40)
 	for i := range values {
 		values[i] = uint(rand.Intn(max-min) + min)
 	}
@@ -23,16 +25,25 @@ func main() {
 	sl.PeakStyle = []glint.StyleOption{glint.Color("green")}
 
 	// Start up a timer that adds values
+	lastValue := uint32(values[len(values)-1])
 	go func() {
 		for {
 			time.Sleep(100 * time.Millisecond)
-			sl.Append(uint(rand.Intn(max-min) + min))
+			value := uint(rand.Intn(max-min) + min)
+			sl.Append(value)
+			atomic.StoreUint32(&lastValue, uint32(value))
 		}
 	}()
 
 	d := glint.New()
 	d.Append(
-		sl,
+		glint.Layout(
+			glint.Layout(glint.Text("requests/second")).MarginRight(2),
+			glint.Layout(sl).MarginRight(2),
+			glint.TextFunc(func(h, w uint) string {
+				return fmt.Sprintf("%d req/s", atomic.LoadUint32(&lastValue))
+			}),
+		).Row(),
 	)
 	d.Render(context.Background())
 }
